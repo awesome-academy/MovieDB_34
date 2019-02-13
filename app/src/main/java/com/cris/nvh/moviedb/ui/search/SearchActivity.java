7affa9f01be53c1cb34c1864c515a15520489e03
hieunv <toiyeuthethao1997@gmail.com>
2019-02-13 10:38:57 +0700

    handle event search

--- app/src/main/java/com/cris/nvh/moviedb/ui/search/SearchActivity.java
+++ app/src/main/java/com/cris/nvh/moviedb/ui/search/SearchActivity.java
@@ -6,6 +6,11 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 import com.cris.nvh.moviedb.R;
 import com.cris.nvh.moviedb.adapter.CategoryAdapter.CategoryViewHolder.MoviesAdapter;
@@ -26,6 +31,9 @@ public class SearchActivity extends AppCompatActivity implements MoviesAdapter.M
         SearchNavigator {
     private ActivitySearchBinding mBinding;
     private SearchViewModel mViewModel;
+    private String mInput = "";
+    private String mType = "movie";
+    private int currentItem, totalItem, scrollOutItem;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,6 +42,8 @@ public class SearchActivity extends AppCompatActivity implements MoviesAdapter.M
         initViewModel();
         mBinding.setSearchVM(mViewModel);
         mBinding.recyclerSearch.setAdapter(new MoviesAdapter(this));
+        setupSearchView();
+        setupScrollListener(mBinding.recyclerSearch);
     }
 
     @Override
@@ -62,10 +72,6 @@ public class SearchActivity extends AppCompatActivity implements MoviesAdapter.M
         onBackPressed();
     }
 
-    public static Intent getIntent(Context context) {
-        return new Intent(context, SearchActivity.class);
-    }
-
     private void initViewModel() {
         FavoriteReaderDBHelper dbHelper = new FavoriteReaderDBHelper(this);
         MovieRepository movieRepository = MovieRepository.getInstance(
@@ -73,5 +79,50 @@ public class SearchActivity extends AppCompatActivity implements MoviesAdapter.M
                 RemoteDataSource.getInstance());
         mViewModel = new SearchViewModel(movieRepository);
         mViewModel.setNavigator(this);
+    }
+
+    private void setupSearchView() {
+        final EditText editSearch = mBinding.editSearch;
+        editSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mInput = charSequence.toString().trim();
+                mViewModel.searchMovie(mType, mInput);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+    }
+
+    private void setupScrollListener(RecyclerView genresRecycler) {
+        genresRecycler.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                currentItem = layoutManager.getChildCount();
+                totalItem = layoutManager.getItemCount();
+                scrollOutItem = layoutManager.findFirstVisibleItemPosition();
+                if (currentItem + scrollOutItem == totalItem) {
+                    mViewModel.increaseCurrentPage();
+                    mViewModel.searchMovie(mType, mInput);
+                }
+            }
+        });
+    }
+
+    public static Intent getIntent(Context context) {
+        return new Intent(context, SearchActivity.class);
     }
 }
